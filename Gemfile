e8a053a294dba1ad218b0ace21813c7ce56d993f
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2014-05-26 17:31:15 +0100

    --Basic setup

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source "http://rubygems.org"
 # Add dependencies required to use your gem here.
 # Example:
 #   gem "activesupport", ">= 2.3.5"
+    gem "coderunner", ">= 0.14.16"
+    gem "budgetcrmod", ">= 0.0.0"
+    gem "command-line-flunky", ">= 1.0.0"
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
---
+++ lib/treasurer.rb
@@ -0,0 +1,103 @@
+
+# A tool for analysing finances and making budget projections
+
+require 'getoptlong'
+
+module CommandLineFlunky
+	
+	STARTUP_MESSAGE = "\n------Treasurer Financial Utility (c) Edmund Highcock------"
+
+	MANUAL_HEADER = <<EOF
+			
+-------------Treasurer Financial Utility Manual---------------
+
+  Written by Edmund Highcock (2014)
+
+NAME
+
+  treasurer
+
+
+SYNOPSIS
+	
+  treasurer <command> [arguments] [options]
+
+
+DESCRIPTION
+
+  Generate a financial report from one or more bank accounts, credit cards
+  etc by analysing internet banking sheets. Simple local files can be used to
+  customise the reports by adding budgets and categories
+  
+EXAMPLES
+
+  treasurer add mybankstatement.csv
+
+  treasurer report 
+
+  treasurer add_folder folder_of_bank_statements
+   
+   
+EOF
+	
+	COMMANDS_WITH_HELP = [
+		['add_file', 'add', 1,  'Import a new internet banking spreadsheet.', ['csv spreadsheet'], []],
+		['add_folder', 'addf', 1,  'Import all internet banking spreadsheets within the given folder .', ['folder'], []],
+
+	]
+	
+
+
+	COMMAND_LINE_FLAGS_WITH_HELP = [
+		#['--boolean', '-b', GetoptLong::NO_ARGUMENT, 'A boolean option'],		
+		#['--formats', '-f', GetoptLong::REQUIRED_ARGUMENT, "A list of formats pertaining to the various input and output files (in the order which they appear), separated by commas. If they are all the same, only one value may be given. If a value is left empty (i.e. there are two commas in a row) then the previous value will be used. Currently supported formats are #{SUPPORTED_FORMATS.inspect}. "],		
+
+		]
+
+	LONG_COMMAND_LINE_OPTIONS = [
+	#["--no-short-form", "", GetoptLong::NO_ARGUMENT, %[This boolean option has no short form]],
+	] 
+		
+	# specifying flag sets a bool to be true
+
+	CLF_BOOLS = []
+
+	CLF_INVERSE_BOOLS = [] # specifying flag sets a bool to be false
+	
+	PROJECT_NAME = 'treasurer'
+		
+	def self.method_missing(method, *args)
+# 		p method, args
+		Treasurer.send(method, *args)
+	end
+	
+	#def self.setup(copts)
+		#CommandLineFlunkyTestUtility.setup(copts)
+	#end
+	
+	SCRIPT_FILE = __FILE__
+end
+
+class Treasurer
+	class << self
+		# This function gets called before every command
+		# and allows arbitrary manipulation of the command
+		# options (copts) hash
+		def setup(copts)
+			# None neededed
+	  end
+		def verbosity
+			2
+		end
+	end
+end
+
+$has_put_startup_message_for_code_runner = true
+require 'coderunner'
+require 'treasurer/commands.rb'
+
+######################################
+# This must be at the end of the file
+#
+require 'command-line-flunky'
+###############################
