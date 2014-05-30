bd4f72b49aa45bbdb99fe4b3c3f0e5de154ffb73
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2014-05-30 13:06:43 +0100

    --Deleted extraneous lines from local_customisations

--- lib/treasurer/local_customisations.rb
+++ lib/treasurer/local_customisations.rb
@@ -1,42 +1,6 @@
 #class CodeRunner::Budget
 module Treasurer::LocalCustomisations
 
-	@excluding = []
-#FUTURE_EXPENDITURE = {
-	##:FirstBank => {
-		##skitrip: { size: 300, date: Date.parse("04/10/2010") },
-		##webhosting: { size: 150, date: Date.parse("01/11/2010") },
-		##cartax: [
-			##{size: 120, date: Date.parse("01/01/2014")},
-		##],
-
-		##counciltax: [
-			##{size: 108, date: Date.parse("12/12/2010")},
-			##{size: 108, date: Date.parse("05/01/2011")},
-			##{size: 108, date: Date.parse("05/02/2011")},
-		##],
-		##heating: [
-			##{size: 250, date: Date.parse("12/12/2013")},
-			##{size: 250, date: Date.parse("05/2/2014")},
-		##],
-
-##},
-  ##:SecondBank => {
-		##doctor: {size: 400, date: Date.parse("01/03/2014") },
-		##windows: {size: 600, date: Date.parse("01/12/2013") },
-##}
-#}
-
-#FUTURE_EXPENDITURE.default = {}
-
-#REGULAR_INCOME = {
-#}
-
-#REGULAR_INCOME.default = {}
-
-#REGULAR_EXPENDITURE = {
-#}
-#REGULAR_EXPENDITURE.default = {}
 
 REGULAR_TRANSFERS = {
 	[:FirstBank, :SecondBank] =>{
@@ -67,27 +31,6 @@ FUTURE_TRANSFERS = {
 FUTURE_TRANSFERS.default = {}
 
 
-## One off expected gains
-#FUTURE_INCOME = {
-	##:FirstBank => {
-		##stipend: [
-			##{ size: 1200*3, date: Date.parse("25/05/2011") },
-			##{ size: 1200*3, date: Date.parse("25/02/2011") },
-			##{ size: 4300, date: Date.parse("20/09/2010") },
-			##{ size: 4300, date: Date.parse("20/12/2010") }
-		##],
-		##expenses: [
-			##{ size: 1085+41, date: Date.parse("1/12/2013") }
-		##],
-
-##},
-  ##:SecondBank => {
-
-##}
-#}
-
-#FUTURE_INCOME.default = {}
-
 
 BUDGETS = {
 	Monthly: {account: :FirstBank, period: [1, :month], monthday: 1, start: nil, end: nil, discretionary: false},
@@ -95,12 +38,6 @@ BUDGETS = {
 	Weekly: {account: :FirstBank, period: [7, :day], monthday: nil, start: nil, end: nil, discretionary: true},
 	WeeklySecondBank: {account: :SecondBank, period: [7, :day], monthday: nil, start: nil, end: nil, discretionary: true},
 	MyHoliday: {account: :SecondBank, period: [1, :day], monthday: nil, start: Date.parse("02/12/2013"), end: Date.parse("2/01/2014"), discretionary: false},
-}
-
-# Transfer budgets
-TRANSFERS = {
-	FirstBankSecondBank: {account: :SecondBank, period: [1, :day], monthday: nil, start: Date.parse("27/06/2013"), end: nil, discretionary: false},
-	#FriendtoSecondBank: {account: :SecondBank, period: [1, :day], monthday: nil, start: Date.parse("27/06/2013"), end: nil, discretionary: false},
 }
 
 def in_date(item)
