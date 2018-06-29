6c7b3ed87eafcc46ef72879ecd7ba66f38acff29
edmundhighcock <edmundhighcock@users.sourceforge.net>
2018-06-29 12:28:13 +0200

    --Added account_choices and external_accounts

---
+++ test/account_choices.rb
@@ -0,0 +1,45 @@
+# coding: utf-8
+{"2010-09-03,CPT,000000,99999999,LTSB CARFAX OXFORDCD 03SEP10,0.00,20.00,156.33"=>
+  {:external_account=>:Cash, :sub_account=>:Withdrawal},
+ "2010-09-03,DD,000000,99999999,VODAFONE LIMITED,0.00,28.61,176.33"=>
+  {:external_account=>:Utilities, :sub_account=>:Phone},
+ "2010-09-03,DEB,000000,99999999,ENDSLEIGH INSURANCE,0.00,24.63,204.94"=>
+  {:external_account=>:MonthlyExpenses, :sub_account=>:Insurance},
+ "2010-09-02,DEB,000000,99999999,MIDCOUNTIES CO-OP,0.00,32.65,229.57"=>
+  {:external_account=>:DailyExpenses, :sub_account=>:Food},
+ "2010-09-01,FPO,000000,99999999,MISS C ADAMS,0.00,150.00,262.22"=>
+  {:external_account=>:Discretionary, :sub_account=>:Gifts},
+ "2010-09-01,DD,000000,99999999,PAYPAL PAYMENT,0.00,5.97,412.22"=>
+  {:external_account=>:Discretionary, :sub_account=>:Computers},
+ "2010-09-01,DEB,000000,99999999,S390-BLACKWELLCD ,0.00,15.00,418.19"=>
+  {:external_account=>:Discretionary, :sub_account=>:Books},
+ "2010-08-25,DEB,000000,99999999,MIDCOUNTIES CO-OP,0.00,52.65,229.57"=>
+  {:external_account=>:DailyExpenses, :sub_account=>:Food},
+ "2010-08-15,DEB,000000,99999999,MIDCOUNTIES CO-OP,0.00,12.65,229.57"=>
+  {:external_account=>:DailyExpenses, :sub_account=>:Food},
+ "2010-08-07,DEB,000000,99999999,MIDCOUNTIES CO-OP,0.00,32.65,229.57"=>
+  {:external_account=>:DailyExpenses, :sub_account=>:Food},
+ "2010-08-01,DEB,000000,99999999,MISS C ADAMS,0.00,150.00,418.19"=>
+  {:external_account=>:Discretionary, :sub_account=>:Gifts},
+ "2010-09-05,CPT,222222,88888888,LNK WINDERMERE,0.00,30.00,860.75"=>
+  {:external_account=>:Cash, :sub_account=>:Withdrawal2},
+ "2010-09-05,FPO,222222,88888888,M H NORRIES,560.00,0.00,890.75"=>
+  {:external_account=>:DailyExpenses, :sub_account=>:Rent},
+ "2010-09-05,DEB,222222,88888888,SAINSBURY'S SMKT,0.00,6.05,1450.75"=>
+  {:external_account=>:DailyExpenses, :sub_account=>:Food},
+ "2010-09-05,DEB,222222,88888888,AL-ANDALUS,0.00,50.00,1456.80"=>
+  {:external_account=>:Discretionary, :sub_account=>:EatingOut},
+ "2010-09-05,DEB,222222,88888888,ANGELS,0.00,11.50,1506.80"=>
+  {:external_account=>:Discretionary, :sub_account=>:EatingOut},
+ "2010-09-01,DEB,222222,88888888,THE MAGGIE ARMS,0.00,48.00,1518.30"=>
+  {:external_account=>:Discretionary, :sub_account=>:EatingOut},
+ "2010-09-01,DEB,222222,88888888,BARLEY MOW,0.00,6.60,1566.30"=>
+  {:external_account=>:Discretionary, :sub_account=>:EatingOut},
+ "2010-08-30,DD,222222,88888888,PAYPAL PAYMENT,0.00,11.91,1572.90"=>
+  {:external_account=>:Discretionary, :sub_account=>:Computers},
+ "2010-08-30,FPO,222222,88888888,MY EMPLOYER,1000.00,0.00,1560.99"=>
+  {:external_account=>:Income, :sub_account=>:Salary},
+ "2010-09-04,DEB,000000,99999999,MARKS & SPENCER,0.00,35.00,121.33"=>
+  {:external_account=>:Discretionary, :sub_account=>:Clothes},
+ "2010-09-06,CPT,222222,88888888,LNK BIRMINGHAM,0.00,40.00,820.75"=>
+  {:external_account=>:Cash, :sub_account=>:Withdrawal2}}
---
+++ test/external_accounts.rb
@@ -0,0 +1,33 @@
+# coding: utf-8
+{:Cash=>
+  {:name=>:Cash,
+   :sym=>"1",
+   :sub_accounts=>
+    {:Withdrawal=>{:name=>:Withdrawal, :sym=>"\e"},
+     :Withdrawal2=>{:name=>:Withdrawal2, :sym=>"1"}}},
+ :Utilities=>
+  {:name=>:Utilities,
+   :sym=>"2",
+   :sub_accounts=>{:Phone=>{:name=>:Phone, :sym=>"1"}}},
+ :MonthlyExpenses=>
+  {:name=>:MonthlyExpenses,
+   :sym=>"3",
+   :sub_accounts=>{:Insurance=>{:name=>:Insurance, :sym=>"1"}}},
+ :DailyExpenses=>
+  {:name=>:DailyExpenses,
+   :sym=>"4",
+   :sub_accounts=>
+    {:Food=>{:name=>:Food, :sym=>"1"}, :Rent=>{:name=>:Rent, :sym=>"2"}}},
+ :Discretionary=>
+  {:name=>:Discretionary,
+   :sym=>"5",
+   :sub_accounts=>
+    {:Gifts=>{:name=>:Gifts, :sym=>"1"},
+     :Computers=>{:name=>:Computers, :sym=>"2"},
+     :Books=>{:name=>:Books, :sym=>"3"},
+     :EatingOut=>{:name=>:EatingOut, :sym=>"4"},
+     :Clothes=>{:name=>:Clothes, :sym=>"5"}}},
+ :Income=>
+  {:name=>:Income,
+   :sym=>"6",
+   :sub_accounts=>{:Salary=>{:name=>:Salary, :sym=>"1"}}}}
