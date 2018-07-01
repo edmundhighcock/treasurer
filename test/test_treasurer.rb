5eda55171f71ab796e32ed0fb36dd0afa7d20f88
edmundhighcock <edmundhighcock@users.sourceforge.net>
2018-07-01 11:43:17 +0200

    --Test suite updated and working

--- test/test_treasurer.rb
+++ test/test_treasurer.rb
@@ -15,6 +15,7 @@ class TestTreaurer < MiniTest::Test
       FileUtils.cp('../external_accounts.rb', '.')
       FileUtils.cp('../account_choices.rb', '.')
 			Treasurer.add_file('../bankaccountstatement.csv', 'FirstBank', {})
+			Treasurer.add_file('../bankaccountstatementduplicate.csv', 'FirstBank', {})
 			Treasurer.status
 			Treasurer.add_file('../otheraccountstatement.csv', 'SecondBank', {})
 			Treasurer.add_folder_of_files('../multiple')
@@ -27,6 +28,7 @@ class TestTreaurer < MiniTest::Test
 			File.open('timing.html', 'w'){|f| printer.print(f, {})}
 			reporter = Treasurer.fetch_reporter(t: Date.parse('2010-09-07'), b: 40, a: 35)
 			reporter.generate_accounts
+      assert_equal(1, reporter.accounts_hash[:FirstBank].runs.find_all{|r| r.description =~ /BLACKW/}.size)
       assert_equal(1032.08, reporter.equities.values[0].balance.round(2))
       assert_equal(1825.28, reporter.equities.values[0].projected_balance(Date.parse('2010-10-09')).round(2))
 		end
